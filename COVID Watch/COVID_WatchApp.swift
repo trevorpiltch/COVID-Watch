fd2a5bc7f038427635c8531441072a546577d132
Trevor <trevor@piltch.com>
2020-10-31 15:31:54 -0400

    Initial Commit

---
+++ COVID Watch/COVID_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_WatchApp.swift
+//  COVID Watch
+//
+//  Created by Trevor Piltch on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct COVID_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVID Watch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID Watch
+//
+//  Created by Trevor Piltch on 10/31/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
